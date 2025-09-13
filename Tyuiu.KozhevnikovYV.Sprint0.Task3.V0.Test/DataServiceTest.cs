7c9a047382709e654f4ab15e1a5f27fad1f1ff10
milio <milio@DESKTOP-22VKOMS>
2025-09-13 16:14:54 +0500

    Task3

---
+++ Tyuiu.KozhevnikovYV.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿namespace Tyuiu.KozhevnikovYV.Sprint0.Task3.V0.Test
+{
+    using Tyuiu.KozhevnikovYV.Sprint0.Task3.V0.Lib;
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}

85db599c282c4469cc823917e55053b2c7b99dca
milio <milio@DESKTOP-22VKOMS>
2025-09-13 15:55:50 +0500

    Task2

---
+++ Tyuiu.KozhevnikovYV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.KozhevnikovYV.Sprint0.Task2.V0.Test
+{
+    using Tyuiu.KozhevnikovYV.Sprint0.Task2.V0.Lib;
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Ярослав";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Ярослав", res);
+        }
+    }
+}
---
+++ Tyuiu.KozhevnikovYV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.KozhevnikovYV.Sprint0.Task2.V0.Lib;
+Console.WriteLine(DataService.GetMessage("Ярослав"));
